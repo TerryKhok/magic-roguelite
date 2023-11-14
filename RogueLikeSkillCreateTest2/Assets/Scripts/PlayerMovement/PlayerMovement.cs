7b0c047a9ee992dd6c048f4d4467d6f1a42a4531
SH <sunziykrsin0814@gmail.com>
2023-11-14 21:35:10 +0900

    ダッシュ機能の追加

--- RogueLikeSkillCreateTest2/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ RogueLikeSkillCreateTest2/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using System.Collections;
 //using System.Numerics;
@@ -5,11 +6,18 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     GameObject Obj;
-    // 速度
     private Rigidbody2D rb = null;
+
     [SerializeField] private float _Speed = 1.0f;
-    [SerializeField] private float _roatation_speed = 100.0f;
-    private Vector3 latestPos;
+    [SerializeField] private float _roatationSpeed = 100.0f;
+    [SerializeField] private float _dashSpeed = 2.0f;
+    [SerializeField] private float _dashCT = 2.0f;
+    [SerializeField] private float _dashtime = 2.0f;
+
+    int hp = 10;
+    float _SpeedPrev;
+    bool _dash = true;
+
 
     private void Start()
     {
@@ -58,38 +66,78 @@ public class PlayerMovement : MonoBehaviour
     {
         float _xSpeed = 0.0f;
         float _ySpeed = 0.0f;
+        int _KeyInputdir = 0;
+        Vector2 movementDirection = Vector2.zero;
 
+        //Debug.Log(_dash);   //==デバッグ==ダッシュ可状態の表示
         if (Input.GetKey("a"))
         { // aキーを押し続けていたら
           // 代入したPositionに対して加算減算を行う
-           _xSpeed = -_Speed;
+            _xSpeed = -_Speed;
+            _KeyInputdir = 90;
         }
         else if (Input.GetKey("d"))
         { // 右キーを押し続けていたら
           // 代入したPositionに対して加算減算を行う
             _xSpeed = _Speed;
+            _KeyInputdir = -90;
         }
         if (Input.GetKey("w"))
         { // 上キーを押し続けていたら
           // 代入したPositionに対して加算減算を行う
             _ySpeed = _Speed;
+            _KeyInputdir = 0;
         }
         else if (Input.GetKey("s"))
         { // 下キーを押し続けていたら
           // 代入したPositionに対して加算減算を行う
             _ySpeed = -_Speed;
+            _KeyInputdir = 180;
         }
         rb.velocity = new Vector2(_xSpeed, _ySpeed);
-        Debug.Log(_xSpeed);
-        Debug.Log(_ySpeed);
-
-
-        Vector2 movementDirection = new Vector2(_xSpeed,_ySpeed);
-        if(movementDirection != Vector2.zero)
+        if (Input.GetKey("a") || Input.GetKey("d") || Input.GetKey("w") || Input.GetKey("s"))
+        {
+            movementDirection = new Vector2(_xSpeed, _ySpeed);
+        }
+        //ダッシュ
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _dash == true && rb.velocity != Vector2.zero)    //「shiftを押した時」に「ダッシュ可状態」であり、「移動している」なら
+        {
+            Dash();
+        }
+        //キャラの回転
+        if (movementDirection != Vector2.zero)  //動いている場合
+                                                // if (transform.rotation.z != _KeyInputdir)
         {
             Quaternion torotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, torotation, _roatation_speed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, torotation, _roatationSpeed * Time.deltaTime);
         }
-       
+    }
+
+    //ダッシュ関数
+    void Dash()
+    {
+        _dash = false;  //ダッシュ不可(ダッシュ中)
+        _SpeedPrev = _Speed; //加速前の速度をバッファに書き込み
+        _Speed = _Speed * _dashSpeed;  //現在の速度にダッシュスピードをかける
+
+        StartCoroutine(Dashstop());
+    }
+
+    IEnumerator Dashstop()
+    {
+        // ダッシュを終わらせる  
+        yield return new WaitForSeconds(_dashtime);
+        _Speed = _SpeedPrev;    //直前の速度に戻す
+
+        // ダッシュクールタイムだけ待機  
+        yield return new WaitForSeconds(_dashCT);
+        _dash = true;
+
+    }
+
+    //ダメージ処理
+    public void PlayerTakeDamage(int dmg)
+    {
+        hp -= dmg;
     }
 }
